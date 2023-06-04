a2f670124d86e143dcdc9c8b79ecf0260cb6a417
kevin-pek <76090938+kevin-pek@users.noreply.github.com>
2023-06-05 00:28:20 +0800

    disable horizontal scroll and cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,17 @@
 'use client'
 
-import * as THREE from 'three'
 import Navbar from '@/components/dom/Navbar'
 import { Loader } from '@react-three/drei'
-import { Canvas, createRoot, events, extend } from '@react-three/fiber'
+import { Canvas } from '@react-three/fiber'
 import './global.css'
 import Index from '@/routes/Index'
 import HeroDom from '@/components/dom/Hero'
 import SkillsDom from '@/components/dom/Skills'
 import AboutDom from '@/components/dom/About'
 import ExperienceDom from '@/components/dom/Experience'
-import { useEffect } from 'react'
 import { Lenis } from '@studio-freight/react-lenis'
 
-extend(THREE)
-
 export default function Page() {
-
   return (
     <Lenis root>
       <Canvas style={{ position: 'fixed' }}>
