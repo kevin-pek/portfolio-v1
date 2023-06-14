6644990602efdfbadf5cb55960fd3c2d8468f62b
kevin-pek <76090938+kevin-pek@users.noreply.github.com>
2023-06-15 03:37:30 +0800

    edit resume

--- src/components/dom/Experience.tsx
+++ src/components/dom/Experience.tsx
@@ -15,7 +15,7 @@ export default function Experience() {
           <div className="text-xl">Want a quick reference?<br />Download my resume!</div>
           <a
             target='_blank'
-            href='kevin-resume.pdf'
+            href='kevin-cv.pdf'
             className='my-auto h-fit w-fit rounded-3xl border-white stroke-white p-2 hover:bg-gray-500'
           >
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-download"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line x1="12" x2="12" y1="15" y2="3"></line></svg>
